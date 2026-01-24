939c07085b0477d735c8e7719191465765d381c8
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-01-24 18:45:31 +0530

    solved the issue with error objeccts and server error 404

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -1,7 +1,66 @@
-const {StatusCodes} = require('http-status-codes');
+// const {StatusCodes} = require('http-status-codes');
 
-const {AirplaneService} = require('../services');
+// const {AirplaneService} = require('../services');
 
+
+// async function createAirplane(req, res) {
+//     console.log('createAirplane called');
+//     try {
+//         const airplane = await AirplaneService.createAirplane({
+//             modelNumber: req.body.modelNumber,
+//             capacity: req.body.capacity
+//         });
+//         return res
+//         .status(StatusCodes.CREATED)
+//         .json({
+//             success: true,
+//             message: 'Airplane created successfully',
+//             data: airplane,
+//             error: {}
+//         });
+//     } catch (error) {
+//         return res
+//         .status(StatusCodes.INTERNAL_SERVER_ERROR)
+//         .json({
+//             success: false,
+//             message: 'Something went wrong while creating airplane',
+//             data: {},
+//             error: error
+//         });
+//     }
+// }
+// async function getAirplanes(req, res) {
+//     try {
+//         const airplanes = await AirplaneService.getAirplanes(req.query);
+//         return res
+//         .status(StatusCodes.OK)
+//         .json({
+//             success: true,
+//             message: 'Successfully fetched airplanes',
+//             data: airplanes,
+//             error: {}
+//         });
+//     } catch (error) {
+//         return res
+//         .status(StatusCodes.INTERNAL_SERVER_ERROR)
+//         .json({
+//             success: false,
+//             message: 'Something went wrong while fetching airplanes',
+//             data: {},
+//             error: error
+//         });
+//     }
+// }
+
+// module.exports = {
+//     createAirplane,
+//     getAirplanes
+// }
+
+const { StatusCodes } = require('http-status-codes');
+
+const { AirplaneService } = require('../services');
+const { SuccessResponse, ErrorResponse } = require('../utils/common');
 
 async function createAirplane(req, res) {
     console.log('createAirplane called');
@@ -10,49 +69,32 @@ async function createAirplane(req, res) {
             modelNumber: req.body.modelNumber,
             capacity: req.body.capacity
         });
-        return res
-        .status(StatusCodes.CREATED)
-        .json({
-            success: true,
-            message: 'Airplane created successfully',
-            data: airplane,
-            error: {}
-        });
+        SuccessResponse.data = airplane;
+        SuccessResponse.message = 'Airplane created successfully';
+        return res.status(StatusCodes.CREATED).json(SuccessResponse);
     } catch (error) {
-        return res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({
-            success: false,
-            message: 'Something went wrong while creating airplane',
-            data: {},
-            error: error
-        });
+        console.log(error);
+        ErrorResponse.error = error;
+        ErrorResponse.message = 'Something went wrong while creating airplane';
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
     }
 }
+
 async function getAirplanes(req, res) {
     try {
         const airplanes = await AirplaneService.getAirplanes(req.query);
-        return res
-        .status(StatusCodes.OK)
-        .json({
-            success: true,
-            message: 'Successfully fetched airplanes',
-            data: airplanes,
-            error: {}
-        });
+        SuccessResponse.data = airplanes;
+        SuccessResponse.message = 'Successfully fetched airplanes';
+        return res.status(StatusCodes.OK).json(SuccessResponse);
     } catch (error) {
-        return res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({
-            success: false,
-            message: 'Something went wrong while fetching airplanes',
-            data: {},
-            error: error
-        });
+        console.log(error);
+        ErrorResponse.error = error;
+        ErrorResponse.message = 'Something went wrong while fetching airplanes';
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(ErrorResponse);
     }
 }
 
 module.exports = {
     createAirplane,
     getAirplanes
-}
+};
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,13 +1,24 @@
+// const express = require('express');
+
+// const { InfoController } = require('../../controllers');
+
+// const airplaneRoutes = require('./airplane-routes');
+
+// const router = express.Router();
+
+
+// router.use('/airplanes', airplaneRoutes);
+// router.get('/info', InfoController.info);
+
+// module.exports = router;
+
 const express = require('express');
 
-const { InfoController } = require('../../controllers');
-
-const airplaneRoutes = require('./airplane-routes');
-
+const { AirplaneController } = require('../../controllers');
+const { AirplaneMiddlewares } = require('../../middlewares');
 const router = express.Router();
 
-
-router.use('/airplanes', airplaneRoutes);
-router.get('/info', InfoController.info);
+router.post('/airplanes', AirplaneMiddlewares.validateCreateRequest, AirplaneController.createAirplane);
+router.get('/airplanes', AirplaneController.getAirplanes);
 
 module.exports = router;
