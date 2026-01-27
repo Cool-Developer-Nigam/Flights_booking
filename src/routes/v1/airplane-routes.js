9577042eb68f6d6dad6c087100adf9445594aad4
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-01-27 22:51:26 +0530

    added get all for all the planes

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
 const { AirplaneController } = require('../../controllers');
+const {AirplaneMiddleware} = require('../../middlewares');
 const router = express.Router();
 
-router.post('/airplanes', AirplaneController.createAirplane);
+router.post('/airplanes', 
+    AirplaneMiddleware.validateCreateAirplane, 
+    AirplaneController.createAirplane);
+    
 router.get('/airplanes', AirplaneController.getAirplanes);
 
 module.exports = router;
