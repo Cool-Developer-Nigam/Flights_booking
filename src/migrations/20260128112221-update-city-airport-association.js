37afcddc613638b732d08e3079c95fbccbaf78f2
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-01-28 20:30:08 +0530

    added the one to manny feature

---
+++ src/migrations/20260128112221-update-city-airport-association.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const { on } = require('nodemon');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addConstraint('Airports',{
+      type: 'FOREIGN KEY',
+      name:'city_fkey_constraint',
+      fields: ['cityId'],
+      references:{
+        table: 'Cities',
+        field: 'id'
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    });
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+
+    await queryInterface.removeConstraint('Airports','city_fkey_constraint');
+  }
+};
